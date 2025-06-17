86eb18e3bef9adf472707fd75a8d45d0632ed8a5
monika2416090 <mithunm.it2023@citchennai.net>
2025-06-17 22:29:20 +0530

    url change in the dashboard

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -244,7 +244,7 @@ function AdminDashboard() {
 
   const copyRegForm = () => {
     const formattedEventName = EventName.replace(/\s+/g, '-');
-    const formLink = `https://events.aurelionfutureforge.com/${formattedEventName}/book-register/${selectedEvent}`;
+    const formLink = `https://aurelionfutureforge.com/stagyn/${formattedEventName}/book-register/${selectedEvent}`;
     if (formLink) {
       navigator.clipboard.writeText(formLink);
       toast.success("Link copied to clipboard!");
